b2ce682a16cdbf540ad77a00ed0094bb9d3e942e
Edwin Ade <edwinmbonyjr@gmail.com>
2023-08-28 16:33:18 +0100

    Solved task 13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - counts the number of nodes with children in a binary tree
+ * @tree: the binary tree
+ * Return: the number of node
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t nodes = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left && tree->right)
+	{
+		nodes++;
+		return (nodes + binary_tree_nodes(tree->left) + 
+		binary_tree_nodes(tree->right));
+	}
+	else if (tree->left)
+	{
+		nodes++;
+		return (nodes + binary_tree_nodes(tree->left));
+	}
+	else if (tree->right)
+	{
+		nodes++;
+		return (nodes + binary_tree_nodes(tree->right));
+	}
+	return (0);
+}
