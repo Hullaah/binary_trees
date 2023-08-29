d2707f416d183120a215dfe6696ad404cf38c268
Edwin Ade <edwinmbonyjr@gmail.com>
2023-08-29 16:06:02 +0100

    Solved tasks 15, 17 and 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_uncle - returns uncle of a binary tree node
+ * @node: the node to be checked
+ * Return: the uncle of the node
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *parent, *grandparent;
+
+	if (!node->parent)
+		return (NULL);
+	parent = node->parent;
+	if (!parent->parent)
+		return (NULL);
+	grandparent = parent->parent;
+	if (!grandparent->left || !grandparent->right)
+		return (NULL);
+	if (parent == grandparent->right)
+		return (grandparent->left);
+	return (grandparent->right);
+}
