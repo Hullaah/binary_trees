cfedf719edd3443262be25effb5c9729a5f55d07
hullaah <umaradelo1.247@gmail.com>
2023-08-30 17:04:05 +0100

    Solved task 100

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,41 @@
+#include "binary_trees.h"
+/**
+ * depth - calculates the depth of a node
+ * @node: node whose depth is to be calculated
+ * Return: depth of node
+*/
+size_t depth(const binary_tree_t *node)
+{
+	return (node && node->parent ? 1 + depth(node->parent) : 0);
+}
+/**
+ * binary_trees_ancestor -  finds the lowest common ancestor of two nodes
+ * @first: first node
+ * @second: second node
+ * Return: lowest common ancestor of first and second nodes
+*/
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+const binary_tree_t *second)
+{
+	int distance;
+	binary_tree_t *ancestor;
+
+	if (!first || !second)
+		return (NULL);
+	distance = depth(first) - depth(second);
+	if (distance < 0)
+	{
+		ancestor = first->parent;
+		distance = -distance;
+		for (int i = 1; i < distance; i++)
+			ancestor = ancestor->parent;
+
+	}
+	else
+	{
+		ancestor = second->parent;
+		for (int i = 1; i < distance; i++)
+			ancestor = ancestor->parent;
+	}
+	return (ancestor);
+}
